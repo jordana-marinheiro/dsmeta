991f3cd6dbebdd35f155740eb97729cda06f1cb8
Jordana <jordanamarinheiro@gmail.com>
2022-07-11 22:27:35 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,15 @@
+import NotificationButton from "./assets/img/components/notificationButton";
+
+
 function App() {
  return(
+    <>
   <h1>Olá mundo!</h1>
+  <NotificationButton />
+  <NotificationButton />
+  
+  </>
+  
  )
 }
 
