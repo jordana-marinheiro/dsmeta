421ca26d6ee77b11f0576b48a516e6ce4982cad2
Jordana <jordanamarinheiro@gmail.com>
2022-07-15 18:45:03 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./componenents/header"
 import SalesCard from "./componenents/SalesCard"
 
@@ -5,6 +7,7 @@ import SalesCard from "./componenents/SalesCard"
 function App() {
  return(
   <>
+  <ToastContainer />
   <Header />
   <main>
         <section id="sales">
--- frontend/src/componenents/SalesCard/index.tsx
+++ frontend/src/componenents/SalesCard/index.tsx
@@ -22,7 +22,6 @@ function SalesCard() {
         const dmin = minDate.toISOString().slice(0,10);
         const dmax = maxDate.toISOString().slice(0,10);
 
-        console.log(minDate);
 
         axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
             .then(response => {
